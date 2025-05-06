7bb7e30bb741fc3f41b99e08e82cd6f05a518ab1
Mykhailo Lapchynskyi <mykhailolapchynskyi@Dikaas-MacBook-Pro.local>
2025-05-06 16:22:30 +0200

    file formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
-import { reactRouter } from '@react-router/dev/vite';
-import svgr from 'vite-plugin-svgr';
 import path from 'path';
+
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
