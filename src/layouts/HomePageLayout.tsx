7933f1424847fab7297e2a07f5e344ff63677d13
Mykhailo <135013788+mykhailonl@users.noreply.github.com>
2025-05-06 16:29:37 +0200

    added desktop layout

--- src/layouts/HomePageLayout.tsx
+++ src/layouts/HomePageLayout.tsx
@@ -1,10 +1,27 @@
 import { Outlet } from 'react-router-dom';
 
-import { MainLayout } from '../components/common/MainLayout';
 import { HomeHeader } from '../components/common/HomeHeader';
 import { HomeMenuBar } from '../components/common/HomeMenuBar';
+import { MainLayout } from '../components/common/MainLayout';
+import { SideBar } from '../components/common/SideBar';
+import { useDevice } from '../hooks/useDevice.ts';
+
+import DesktopLayout from './DesktopLayout.tsx';
+
 
 export const HomePageLayout = () => {
+  const { isDesktop } = useDevice();
+
+  if (isDesktop) {
+    return (
+      <DesktopLayout>
+        <SideBar />
+
+        <Outlet />
+      </DesktopLayout>
+    );
+  }
+
   return (
     <MainLayout>
       <HomeHeader />
