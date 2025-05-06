d503d206e0646c58c5c3ba51297b379710a78dd9
Mykhailo Lapchynskyi <mykhailolapchynskyi@Dikaas-MacBook-Pro.local>
2025-05-06 16:20:57 +0200

    added extra icon with type safety

--- src/types/Icons/icons.ts
+++ src/types/Icons/icons.ts
@@ -1,13 +1,14 @@
-import { ReactComponent as HomeIcon } from '../../assets/images/icon-home.svg';
 import { ReactComponent as ArchiveIcon } from '../../assets/images/icon-archive.svg';
-import { ReactComponent as SearchIcon } from '../../assets/images/icon-search.svg';
-import { ReactComponent as TagIcon } from '../../assets/images/icon-tag.svg';
-import { ReactComponent as SettingsIcon } from '../../assets/images/icon-settings.svg';
-import { ReactComponent as PlusIcon } from '../../assets/images/icon-plus.svg';
-import { ReactComponent as ClockIcon } from '../../assets/images/icon-clock.svg';
-import { ReactComponent as LogoIcon } from '../../assets/images/logo.svg';
 import { ReactComponent as LeftArrowIcon } from '../../assets/images/icon-arrow-left.svg';
+import { ReactComponent as ChevronRightIcon } from '../../assets/images/icon-chevron-right.svg';
+import { ReactComponent as ClockIcon } from '../../assets/images/icon-clock.svg';
 import { ReactComponent as DeleteIcon } from '../../assets/images/icon-delete.svg';
+import { ReactComponent as HomeIcon } from '../../assets/images/icon-home.svg';
+import { ReactComponent as PlusIcon } from '../../assets/images/icon-plus.svg';
+import { ReactComponent as SearchIcon } from '../../assets/images/icon-search.svg';
+import { ReactComponent as SettingsIcon } from '../../assets/images/icon-settings.svg';
+import { ReactComponent as TagIcon } from '../../assets/images/icon-tag.svg';
+import { ReactComponent as LogoIcon } from '../../assets/images/logo.svg';
 
 // Dict to store icon names as keys and imported components as values
 export const iconComponents = {
@@ -21,6 +22,7 @@ export const iconComponents = {
   logo: LogoIcon,
   leftArrow: LeftArrowIcon,
   delete: DeleteIcon,
+  chevronRight: ChevronRightIcon,
 };
 
 // Type for icons based on dict keys
@@ -28,3 +30,6 @@ export type IconName = keyof typeof iconComponents;
 
 // Type used in mapping inside Menu Bar Icons to get text values with safety;
 export type MenuIconName = Extract<IconName, 'home' | 'search' | 'archive' | 'tag' | 'settings'>;
+
+// Type used in mapping inside Side Bar Icons to get text values with safety;
+export type SideBarIconName = Extract<IconName, 'home' | 'archive'>;
