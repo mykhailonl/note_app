c2e26199d4a67d8b2bff94da1936755f4ee810a5
Mykhailo <135013788+mykhailonl@users.noreply.github.com>
2025-05-20 12:40:20 +0200

    added svg icons for radio buttons, adjusted style behavior for the rest of the icons, added Icon Types

--- src/types/Icons/IconsType.ts
+++ src/types/Icons/IconsType.ts
@@ -3,16 +3,29 @@ import { ReactComponent as LeftArrowIcon } from '../../assets/images/icon-arrow-
 import { ReactComponent as ChevronRightIcon } from '../../assets/images/icon-chevron-right.svg';
 import { ReactComponent as ClockIcon } from '../../assets/images/icon-clock.svg';
 import { ReactComponent as DeleteIcon } from '../../assets/images/icon-delete.svg';
+import { ReactComponent as FontMonospaceIcon } from '../../assets/images/icon-font-monospace.svg';
+import { ReactComponent as FontSansSerifIcon } from '../../assets/images/icon-font-sans-serif.svg';
+import { ReactComponent as FontSerifIcon } from '../../assets/images/icon-font-serif.svg';
+import { ReactComponent as FontIcon } from '../../assets/images/icon-font.svg';
 import { ReactComponent as GoogleIcon } from '../../assets/images/icon-google.svg';
 import { ReactComponent as HideInputIcon } from '../../assets/images/icon-hide-password.svg';
 import { ReactComponent as HomeIcon } from '../../assets/images/icon-home.svg';
 import { ReactComponent as InfoIcon } from '../../assets/images/icon-info.svg';
+import { ReactComponent as LockIcon } from '../../assets/images/icon-lock.svg';
+import { ReactComponent as LogoutIcon } from '../../assets/images/icon-logout.svg';
+import { ReactComponent as DarkThemeIcon } from '../../assets/images/icon-moon.svg';
 import { ReactComponent as PlusIcon } from '../../assets/images/icon-plus.svg';
 import { ReactComponent as SearchIcon } from '../../assets/images/icon-search.svg';
 import { ReactComponent as SettingsIcon } from '../../assets/images/icon-settings.svg';
 import { ReactComponent as ShowInputIcon } from '../../assets/images/icon-show-password.svg';
+import { ReactComponent as LightThemeIcon } from '../../assets/images/icon-sun.svg';
+import { ReactComponent as SystemThemeIcon } from '../../assets/images/icon-system-theme.svg';
 import { ReactComponent as TagIcon } from '../../assets/images/icon-tag.svg';
 import { ReactComponent as LogoIcon } from '../../assets/images/logo.svg';
+import { ReactComponent as RadioDarkIcon } from '../../assets/images/radio-dark.svg';
+import { ReactComponent as RadioDarkCheckedIcon } from '../../assets/images/radio-darkChecked.svg';
+import { ReactComponent as RadioLightIcon } from '../../assets/images/radio-light.svg';
+import { ReactComponent as RadioLightCheckedIcon } from '../../assets/images/radio-lightChecked.svg';
 
 // Dict to store icon names as keys and imported components as values
 export const iconComponents = {
@@ -31,6 +44,19 @@ export const iconComponents = {
   show: ShowInputIcon,
   hide: HideInputIcon,
   info: InfoIcon,
+  lightTheme: LightThemeIcon,
+  font: FontIcon,
+  lock: LockIcon,
+  logout: LogoutIcon,
+  fontMonospace: FontMonospaceIcon,
+  fontSansSerif: FontSansSerifIcon,
+  fontSerif: FontSerifIcon,
+  darkTheme: DarkThemeIcon,
+  systemTheme: SystemThemeIcon,
+  radioLight: RadioLightIcon,
+  radioLightChecked: RadioLightCheckedIcon,
+  radioDark: RadioDarkIcon,
+  radioDarkChecked: RadioDarkCheckedIcon,
 };
 
 // Type for icons based on dict keys
@@ -44,3 +70,21 @@ export type SideBarIconName = Extract<IconName, 'home' | 'archive'>;
 
 // Type used in mapping inside Right Side Bar Icons to get text values with safety;
 export type RightSideBarIconName = Extract<IconName, 'delete' | 'archive'>;
+
+// Type used in mapping inside Right Side Bar Icons to get text values with safety;
+export type BackButtonIconName = Extract<IconName, 'leftArrow'>;
+
+// Type used in mapping inside Font Theme settings Icons to get text values with safety;
+export type FontThemeIconName = Extract<IconName, 'fontSansSerif' | 'fontSerif' | 'fontMonospace'>;
+
+// Type used in mapping inside Color Theme settings Icons to get text values with safety;
+export type ColorThemeIconName = Extract<IconName, 'lightTheme' | 'darkTheme' | 'systemTheme'>;
+
+// Type used in mapping inside SettingsPage Icons to get text values with safety;
+export type SettingsIconName = Extract<IconName, 'lightTheme' | 'font' | 'lock' | 'logout'>;
+
+// Type used for radio buttons inside settings
+export type RadioButtonsIconName = Extract<
+  IconName,
+  'radioLight' | 'radioLightChecked' | 'radioDark' | 'radioDarkChecked'
+>;
